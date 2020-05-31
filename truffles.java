b09758f54c480bb229f421d872287126c5930469
buzardh <buzardh@wwu.edu>
2020-05-31 22:32:58 +0000

    Update truffles.java

--- truffles.java
+++ truffles.java
@@ -30,7 +30,7 @@ class truffles {
                     dist[i][j] = Math.max(arr[i-1][j], arr[i-1][j-1]);
                 }
                 else if (j== n-1) {
-                    dist[i][j] = Math.max(arr[i-1][j], arr[i-1][j-1]);
+                    dist[i][j] = Math.max(arr[i-1][j], arr[i-1][j+1]);
                 }
                 else {
                     dist[i][j] = Math.max(arr[i-1][j], Math.max(arr[i-1][j-1], arr[i-1][j+1])); 
