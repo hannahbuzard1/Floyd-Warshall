ff38d30a4cb2ce9b7bcad80e1bfeaed918f58953
buzardh <buzardh@wwu.edu>
2020-06-02 01:37:51 +0000

    Update truffles.java

--- truffles.java
+++ truffles.java
@@ -10,7 +10,7 @@ class truffles {
     
     static int getValue(int arr[][], int m, int n) { 
         int[][] pred = new int[m+1][n];
-        for (int i = 0; i< m + 1; i++) {
+        for (int i = 0; i< m ; i++) {
             for (int j =0; j< n; j++) {
                 pred[i][j] = 0;
             }
