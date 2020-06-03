dfc01fd39201272a17cb1f508981f65cfdbe76b0
buzardh <buzardh@wwu.edu>
2020-06-03 22:58:15 +0000

    Update truffles2.java

--- truffles2.java
+++ truffles2.java
@@ -66,7 +66,7 @@ public static void floydWarshall(int graph[][], int V) {
             System.out.println("");
         }
         int max = 0;
-        getPath(p,2,7);
+        getPath(p,2,6);
         for (i = 0; i < currentpath.size(); i++) {
              max = max + currentpath.get(i);
         }
