a80ded54a8c368d2139644c57fe634fdb4b82bbc
buzardh <buzardh@wwu.edu>
2020-06-03 20:51:59 +0000

    Update truffles2.java

--- truffles2.java
+++ truffles2.java
@@ -150,7 +150,7 @@ public class truffles2 {
                 }
             }
         }
-        Collections.addAll(globalnodes,array)
+        Collections.addAll(globalnodes,array);
         //call Floyd Warshall with weight matrix
         FloydWarshall(weights, nodes);
     }
