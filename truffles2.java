c0618efa16e2348ccc94983ba8aa55b619b620fe
buzardh <buzardh@wwu.edu>
2020-06-06 04:05:29 +0000

    Update truffles2.java

--- truffles2.java
+++ truffles2.java
@@ -129,13 +129,15 @@ public class truffles2 {
             Scanner input = new Scanner(new File(filename));
             for(int i = 0; i < rowcount; ++i) {
                 for(int j = 0; j < colcount; ++j) {
-                    if(input.hasNextInt()) {
+                    if(input.hasNext()) {
                         int val = i + 1; //need this for one-indexed representation
-                        System.out.print("Next int: ");
-                        System.out.println(input.nextInt());
-                        nodelist[count] = input.nextInt();
-                        nodeloc[count] = "[" + val + ", " + j + "]";
-                        count++;
+                        if(input.next() != ' ') {
+                            System.out.print("Next int: ");
+                            System.out.println(input.nextInt());
+                            nodelist[count] = input.nextInt();
+                            nodeloc[count] = "[" + val + ", " + j + "]";
+                            count++;
+                        }
                     }
                 }
             }
